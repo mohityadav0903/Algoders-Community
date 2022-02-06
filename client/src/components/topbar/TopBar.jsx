585508458c7a7b7c1118ef029360bbf763a6bc16
Krishnkant <Krishnkantpachori50@gmail.com>
2022-02-06 20:05:50 +0530

    change in brand

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -48,8 +48,8 @@ const TopBar = () => {
 //   </div>;
 <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
   <div className="container-fluid ">
-    <a className="navbar-brand " href="/">
-      Algoders Community</a>
+    <Link className="navbar-brand " to="/">
+      Algoders Community</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
