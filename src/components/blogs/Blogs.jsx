4691b19c5d76acc5c4b044a303709e01d93a9d2a
Mohit yadav <elitemohityadav@gmail.com>
2022-03-02 17:08:08 +0530

    scroll effect added

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
-import React,{useEffect} from 'react';
+import React from 'react';
 import Posts from '../posts/Posts';
 import './Blogs.css'
-=======
-import React from "react";
-import Posts from "../posts/Posts";
-import "./Blogs.css";
->>>>>>> 9049b147422f9e8c533066057cb934799ff14f0e
 
 
 const Blogs = () => {
-<<<<<<< HEAD
-  
-  return <div className='blogs' ><Posts/></div>;
-=======
   return (
     <div className="blogs">
       <Posts />
     </div>
   );
->>>>>>> 9049b147422f9e8c533066057cb934799ff14f0e
 };
 
 export default Blogs;
