42cc5d9a962c8427b75ca8ec36415d28a3d75306
Pranav <pranavvermaofiicial@gmail.com>
2022-03-16 12:38:32 +0530

    #3pranav2

--- src/components/joinus/Join.jsx
+++ src/components/joinus/Join.jsx
@@ -1,13 +1,8 @@
-import React, { useEffect } from 'react';
-import Aos from "aos";
-import "aos/dist/aos.css";
+import React from 'react';
 
 function Join() {
-  useEffect(()=>{
-    Aos.init({duration:1000})
-  })
   return (
-    <div className='bg-black' data-aos="fade-up">
+    <div className='bg-black'>
            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path
             fill="#f2f6fc"
@@ -22,18 +17,18 @@ function Join() {
     </div>
   </div>
     <div className="row align-items-center mb-5 ">
-            <div className="col-lg-6  text-center" data-aos="fade-up"><i className="fa fa-leaf fa-2x mb-3 text-primary"></i>
+            <div className="col-lg-6  text-center"><i className="fa fa-leaf fa-2x mb-3 text-primary"></i>
               <h2 className="font-weight-light text-white fw-bolder">What You Can Get ?</h2>
               <p className="font-italic  mb-4 text-primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, necessitatibus Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque, vel suscipit voluptate saepe laboriosam unde fuga dolore obcaecati in, sed voluptatem corporis eligendi! Beatae, labore!</p>
               
               <button href="#"
                 className="btn btn-light px-3 rounded-pill shadow-sm">Apply Here</button>
             </div>
-            <div className="col-lg-5 px-5 mx-auto order-sm-first my-3" data-aos="flip-left"><img
+            <div className="col-lg-5 px-5 mx-auto order-sm-first my-3"><img
              
           src="https://bootstrapmade.com/demo/templates/Vesperr/assets/img/counts-img.svg" alt="" className="img-fluid mb-4 mb-lg-0" /></div>
           </div>
-       <div className="row" data-aos="fade-down">
+       <div className="row">
   <div className="col-sm-4 my-3">
     <div className="card text-center">
       <div className="card-body">
