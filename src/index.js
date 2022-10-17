c7cd0681f254744309bfdfb748d323393a8f22a8
Gbemisola Kotoye <barkty@yahoo.com>
2022-10-17 10:22:44 +0100

    fix: Add formik to enhance form validation and error response

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import App from './App';
 import './index.css';
 import {BrowserRouter} from 'react-router-dom';
 import { ContextProvider } from './context/Context';
+import { SnackbarProvider } from 'notistack';
 
 
-ReactDOM.render(<ContextProvider><BrowserRouter><App /></BrowserRouter></ContextProvider>,document.getElementById('root'));
+ReactDOM.render(
+    <ContextProvider>
+        <BrowserRouter>
+            <SnackbarProvider>
+                <App />
+            </SnackbarProvider>
+        </BrowserRouter>
+    </ContextProvider>
+    ,document.getElementById('root'));
 
 
 
