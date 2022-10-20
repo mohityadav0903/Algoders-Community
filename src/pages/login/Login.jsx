d01b1996041092d05555f23dd64508c5f96cc574
Mohit yadav <83297933+mohityadav0903@users.noreply.github.com>
2022-10-20 11:26:01 +0530

    Revert "Add formik to enhance form validation and error response"

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,87 +5,43 @@ import { Context } from "../../context/Context";
 import "./Login.css";
 import SubmitButton from "../../components/buttons/SubmitButton";
 import SpinnerButton from "../../components/buttons/SpinnerButton";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import { useSnackbar } from "notistack";
-import Spinner from "react-bootstrap/Spinner";
-
 
 const Login = () => {
+  const userRef = useRef();
+  const passwordRef = useRef();
   const { dispatch } = useContext(Context);
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const { enqueueSnackbar } = useSnackbar();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const validationSchema = Yup.object().shape({
-    username: Yup.string()
-      .required("Email is required"),
-    password: Yup.string()
-      .min(8, "Password must be 8 characters or more")
-      .required("Password is required!"),
-  });
-
-  const handleSuccess = (message) => {
-    enqueueSnackbar(message, {
-      variant: "success",
-      anchorOrigin: {
-        vertical: "top",
-        horizontal: "right",
-      },
-    });
-  };
-const handleFail = (message) => {
-    enqueueSnackbar(message, {
-      variant: "error",
-      anchorOrigin: {
-        vertical: "top",
-        horizontal: "right",
-      },
-    });
-  };
-
-  const handleSignIn = async (values) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if (!userRef.current.value || !passwordRef.current.value) {
+      setError("Please fill all the required inforamtion");
+      setLoading(false);
+      return;
+    }
     dispatch({ type: "LOGIN_START" });
     try {
       const res = await axios.post(
         "https://algo-backend.herokuapp.com/api/auth/login",
         {
-          username: values.username,
-          password: values.password
+          username: userRef.current.value,
+          password: passwordRef.current.value,
         }
       );
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      setLoading(false);
       console.log(res);
-      resetForm({ username: username, password: password })
     } catch (error) {
+      setLoading(false);
       const { status } = error.response;
-      resetForm({ username: username, password: password })
       dispatch({ type: "LOGIN_FAILURE" });
       if (status === 401) {
-        handleFail("Invalid username & password, please try again")
-      } else {
-        handleFail("Something went wrong")
+        setError("Invalid username & password, please try again");
       }
     }
-  };
-
-  const {isSubmitting, handleBlur, handleChange, values: {username, password}, resetForm, errors: { username: usernameError, password: passwordError}, handleSubmit} = useFormik({
-    initialValues: {
-      username: "",
-      password: "",
-    },
-    enableReinitialize: true,
-    onSubmit: handleSignIn,
-    validationSchema,
-  });
-
-  const error = {
-    color: "red",
-    width: "100%",
-    position: "relative",
-    fontSize: "14px",
-    top: "5px",
-    left: "10px",
-    textAlign: "left",
   };
 
   return (
@@ -98,53 +54,49 @@ const handleFail = (message) => {
             name="username"
             type="text"
             placeholder="Username*"
-            value={username}
-            onBlur={handleBlur}
-            onChange={handleChange}
+            ref={userRef}
           />
-          {usernameError && usernameError ? (
-            <span style={error}>{usernameError}</span>
-          ) : null}
-          <div className="input-wrap">
-            <div className="input-container">
-              <input
-                className="input input-password"
-                name="password"
-                type={passwordVisible ? "text" : "password"}
-                placeholder="Password*"
-                value={password}
-                onBlur={handleBlur}
-                onChange={handleChange}
-              />
-              {passwordVisible ? (
-                <i
-                  className="fa-regular fa-eye"
-                  onClick={() => {
-                    setPasswordVisible(!passwordVisible);
-                  }}
-                ></i>
-              ) : (
-                <i className="fa-regular fa-eye-slash"
-                  onClick={() => {
-                    setPasswordVisible(!passwordVisible);
-                  }}
-                ></i>
-              )}
-            </div>
-              {passwordError && passwordError ? (
-                <span style={error}>{passwordError}</span>
-              ) : null}
+          <div className="input-container">
+            <input
+              className="input input-password"
+              name="password"
+              type={passwordVisible ? "text" : "password"}
+              placeholder="Password*"
+              ref={passwordRef}
+            />
+            {passwordVisible ? (
+              <i
+                class="fa-regular fa-eye"
+                onClick={() => {
+                  setPasswordVisible(!passwordVisible);
+                }}
+              ></i>
+            ) : (
+              <i
+                class="fa-regular fa-eye-slash"
+                onClick={() => {
+                  setPasswordVisible(!passwordVisible);
+                }}
+              ></i>
+            )}
           </div>
           <div className="login-footer">
             <div className="form-text">
               <p>
                 Don't have an account? <Link to="/register">Register</Link>
               </p>
+              {error && <p className="register-error">{error}</p>}
             </div>
 
-            <button className="login-button" type="submit" disabled={isSubmitting}>
-              {isSubmitting ? (<Spinner animation="border" variant="light"/>) : ('Login') }
-            </button>
+            {loading ? (
+              <button className="login-button" type="submit">
+                Loading...
+              </button>
+            ) : (
+              <button className="login-button" type="submit">
+                Login
+              </button>
+            )}
           </div>
         </form>
       </div>
